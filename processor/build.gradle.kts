a82d2c0316379fb4cfd1aff9039037167297ef71
Gaurang <gaurang.shaha@gmail.com>
2023-11-08 21:06:04 +0530

    Added maven publishing script

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    `maven-publish`
 }
 
 group = "com.gaurang.storybook"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -11,4 +12,16 @@ repositories {
 
 dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.0-1.0.13")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.gaurang"
+            artifactId = "storybook"
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
 }
